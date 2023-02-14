9651e72398203d22cdc02eff06baace8e2c4948c
labib fadhlurrahman <labib.fadhlurrahman@kompas..com>
2023-02-14 10:39:34 +0700

    test

--- app/Http/Controllers/CountController.php
+++ app/Http/Controllers/CountController.php
@@ -15,10 +15,12 @@ class CountController extends Controller
         $mode = $request->input('mode');
         $limit = 5;
         $query = '';
-        $count = '';
+        $total_asset = '';
+        $total_label = '';
+        $data = array();
 
         if ($departement_id){
-            if($mode == 'asset'){
+            // if($mode == 'asset'){
                 $query = Asset::leftjoin('departments', 'departments.id', '=', 'assets.departement_id')
                     ->leftjoin('categories', 'categories.id', '=', 'assets.category_id')
                     ->leftjoin('counts', 'counts.id', '=', 'assets.count_id')
@@ -27,22 +29,22 @@ class CountController extends Controller
                     ->limit($limit)
                     ->get(['assets.*','departments.department','categories.category','counts.count']);
 
-                $query_count = Asset::leftjoin('departments', 'departments.id', '=', 'assets.departement_id')
+                $query_count_asset = Asset::leftjoin('departments', 'departments.id', '=', 'assets.departement_id')
                     ->leftjoin('categories', 'categories.id', '=', 'assets.category_id')
                     ->leftjoin('counts', 'counts.id', '=', 'assets.count_id')
                     ->where('departement_id', $departement_id)
                     ->orderBy('assets.created_at', 'DESC')
                     ->limit($limit)
                     ->get(['assets.*','departments.department','categories.category','counts.count']);
-                $count = count($query_count);
-                if($count == 0){
-                    $status = false;
-                    $message = "No Data Avaiable";
-                }else{
-                    $status = true;
-                    $message = "here is data";
-                }
-            }else if($mode == 'upload'){
+                $total_asset = count($query_count_asset);
+                // if($count == 0){
+                //     $status = false;
+                //     $message = "No Data Avaiable";
+                // }else{
+                //     $status = true;
+                //     $message = "here is data";
+                // }
+            // }else if($mode == 'upload'){
                 $query = Asset::join('uploads', 'uploads.asset_id', '=', 'assets.id')
                     ->leftjoin('departments', 'departments.id', '=', 'assets.departement_id')
                     ->leftjoin('categories', 'categories.id', '=', 'assets.category_id')
@@ -72,7 +74,7 @@ class CountController extends Controller
                     ->limit($limit)
                     ->get(['assets.*','departments.department','categories.category','counts.count']);
 
-                $query_count = Asset::join('uploads', 'uploads.asset_id', '=', 'assets.id')
+                $query_count_mutation = Asset::join('uploads', 'uploads.asset_id', '=', 'assets.id')
                     ->leftjoin('departments', 'departments.id', '=', 'assets.departement_id')
                     ->leftjoin('categories', 'categories.id', '=', 'assets.category_id')
                     ->leftjoin('counts', 'counts.id', '=', 'assets.count_id')
@@ -101,15 +103,17 @@ class CountController extends Controller
                     ->limit($limit)
                     ->get(['assets.*','departments.department','categories.category','counts.count']);
 
-                $count = count($query_count);
-                if($count == 0){
-                    $status = false;
-                    $message = "No Data Avaiable";
-                }else{
-                    $status = true;
-                    $message = "here is data";
-                }
-            }else{
+                // $total_label = count($query_count);
+                $total_asset_label = count($query_count_mutation);
+                $total_label = $query;
+                // if($count == 0){
+                //     $status = false;
+                //     $message = "No Data Avaiable";
+                // }else{
+                //     $status = true;
+                //     $message = "here is data";
+                // }
+            // }else{
                 $query_count = Asset::leftjoin('departments', 'departments.id', '=', 'assets.departement_id')
                     ->leftjoin('categories', 'categories.id', '=', 'assets.category_id')
                     ->leftjoin('counts', 'counts.id', '=', 'assets.count_id')
@@ -128,18 +132,20 @@ class CountController extends Controller
                     $status = true;
                     $message = "here is data";
                 }
-            }
+            // }
         }else{
             return response()->json([
                 'status' => false,
                 'message' => 'Departement is null'
             ]);
         }
+        $data['total_asset'] =  $total_asset;
+        $data['total_asset_label'] =  $total_asset_label;
+        $data['list_data_labels'] = $total_label;
         return response()->json([
             "status" => $status,
             "message" => $message,
-            "total_data" => $count,
-            "data" => $query
+            "data" => $data
         ]);
     }
 
@@ -257,7 +263,6 @@ class CountController extends Controller
         return response()->json([
             "status" => $status,
             "message" => $message,
-            "total_data" => $count,
             "data" => $query
         ]);
     }
