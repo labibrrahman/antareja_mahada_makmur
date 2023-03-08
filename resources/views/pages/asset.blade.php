670eacceb28d42375ddd9f10b24e99ba95c523e7
Labib Fadhlurrahman <fadhlurrahmanlabib13@gmail.com>
2023-03-08 20:54:27 +0700

    a

--- resources/views/pages/asset.blade.php
+++ resources/views/pages/asset.blade.php
@@ -347,7 +347,6 @@
         </div>
       </div>
 
-      
       <div class="modal fade" id="updateImage" tabindex="-1" role="dialog" aria-labelledby="updateImage" aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
           <div class="modal-content">
